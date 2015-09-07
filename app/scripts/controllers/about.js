418df8efd25c2f0d8a0d3e22c5f9dac171f56c2e
jens brimberg <jens.brimberg@gmail.com>
2015-09-07 05:17:27 +0200

    Initial Yeoman scaffolding set up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chargenNgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chargenNgApp
+ */
+angular.module('chargenNgApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
