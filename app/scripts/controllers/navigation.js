99559782fdc4a9d6baf92b6f836e7385a57b295e
jens brimberg <jens.brimberg@gmail.com>
2015-09-08 07:07:03 +0200

    fixed navigation.js

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -9,11 +9,28 @@
  */
 angular.module('chargenNgApp')
     .controller('NavCtrl', ['$scope', '$location', function ($scope, $location) {
-        $scope.menus = [{name: 'Mutant', route: '/mutant'},
-                       {name: 'Svavelvinter', route: '/svavelvinter'},
-                       {name: 'DnD 5', route: '/dnd5'},
-                       {name: 'DnD 3.5', route: '/dnd35'},
-                       {name: 'Drakar och Demoner', route: '/dod'}];
+        $scope.menus = [
+            {
+                name: 'Mutant',
+                route: '/mutant'
+            },
+            {
+                name: 'Svavelvinter',
+                route: '/svavelvinter'
+            },
+            {
+                name: 'DnD 5',
+                route: '/dnd5'
+            },
+            {
+                name: 'DnD 3.5',
+                route: '/dnd35'
+            },
+            {
+                name: 'Drakar och Demoner',
+                route: '/dod'
+            }
+        ];
         $scope.getActiveClass = function (location) {
             if (location === $location.path().substr(0, location.length)) {
                 return 'active';
