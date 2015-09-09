2b43820d689361b231b324a4c58a657582c2fa19
jens brimberg <jens.brimberg@gmail.com>
2015-09-09 21:30:33 +0200

    added G-E handling

--- app/scripts/controllers/mutant.js
+++ app/scripts/controllers/mutant.js
@@ -7,6 +7,10 @@
  * # MutantCtrl
  * Controller of the chargenNgApp
  */
+var jsonLog = function (j) {
+    console.log(JSON.stringify(j, null, '\t'));
+};
+
 angular.module('chargenNgApp')
     .controller('MutantCtrl', ['$scope', 'mutantMongoServiceFactory', function ($scope, mutantMongoServiceFactory) {
 
@@ -18,6 +22,7 @@ angular.module('chargenNgApp')
             console.log('not yet implemented');
         };
         $scope.loadCharacter = function (character) {
+            jsonLog(character);
             $scope.activeCharacter = character;
             $scope.create = undefined;
         };
--- app/scripts/services/mutant-mongo-service-factory.js
+++ app/scripts/services/mutant-mongo-service-factory.js
@@ -15,21 +15,29 @@ angular.module('chargenNgApp')
             imm: {
                 name: 'Icke muterad människa',
                 sp: 91,
+                minPrimAttr: 5,
+                maxPrimAttr: 20,
                 description: 'Upper crust'
             },
             mm: {
                 name: 'Muterad människa',
                 sp: 84,
+                minPrimAttr: 3,
+                maxPrimAttr: 18,
                 description: 'Löskefolket'
             },
             psi: {
                 name: 'Psi-mutant',
                 sp: 84,
+                minPrimAttr: 3,
+                maxPrimAttr: 18,
                 description: 'Farliga'
             },
             rbt: {
                 name: 'Robot',
                 sp: 84,
+                minPrimAttr: 3,
+                maxPrimAttr: 18,
                 description: 'Tjänaren och underverket'
             }
         };
@@ -40,19 +48,68 @@ angular.module('chargenNgApp')
                 description: 'jobba m jord o lort'
             }
         };
+        var attrPrim = {
+            STY: {
+                name: 'Styrka',
+                short: 'STY',
+                description: '',
+                value: 5,
+                mod: 0
+            },
+            FYS: {
+                name: 'Fysik',
+                short: 'FYS',
+                description: '',
+                value: 5,
+                mod: 0
+            },
+            STO: {
+                name: 'Storlek',
+                short: 'STO',
+                description: '',
+                value: 5,
+                mod: 0
+            },
+            SMI: {
+                name: 'Smidighet',
+                short: 'SMI',
+                description: '',
+                value: 5,
+                mod: 0
+            },
+            INT: {
+                name: 'Intelligens',
+                short: 'INT',
+                description: '',
+                value: 5,
+                mod: 0
+            },
+            VIL: {
+                name: 'Vilja',
+                short: 'VIL',
+                description: '',
+                value: 5,
+                mod: 0
+            },
+            PER: {
+                name: 'Personlighet',
+                short: 'PER',
+                description: '',
+                value: 5,
+                mod: 0
+            }
+        };
+        var templateChars = [{
+            name: 'one',
+            klass: klasses.imm,
+            job: jobs.bonde,
+            attrPrim
+                }];
 
         // Public API here
         return {
             getCharacters: function () {
-                return [{
-                    name: 'one',
-                    klass: klasses.imm,
-                    job: jobs.bonde
-                }, {
-                    name: 'two',
-                    klass: klasses.psi,
-                    job: jobs.bonde
-                }];
+                return templateChars;
             },
             getClasses: function () {
                 return klasses;
