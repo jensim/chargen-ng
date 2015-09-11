1e1cdec5464d1ad57b4fef69b796734fc0d5ccea
jens brimberg <jens.brimberg@gmail.com>
2015-09-11 07:40:59 +0200

    Added calculations for Erf and SP used

--- app/scripts/controllers/mutant.js
+++ app/scripts/controllers/mutant.js
@@ -26,7 +26,60 @@ angular.module('chargenNgApp')
             $scope.activeCharacter = character;
             $scope.create = undefined;
         };
-        $scope.skillSum = function (attr) {
-            return 3;
+        $scope.skillSum = function (skill) {
+            var timesGE = skill.natural ? 1 : 0;
+            timesGE += skill.valueSp + skill.valueSpFree;
+            var fromGE = timesGE * (skill.attrPrim.value + skill.attrPrim.mod);
+            var fromTrain = skill.valueErf + skill.valueErfFree;
+            console.log(skill.name + ' skillsum: ' + fromGE + fromTrain);
+            return fromGE + fromTrain;
+        };
+        $scope.getUsedSP = function () {
+            var sum = 0;
+            for (skill in $scope.activeCharacter.skills) {
+                sum += $scope.activeCharacter.skills[skill].valueSp;
+            }
+            //TODO:calculate from powers
+            console.error('getUsedSP is not calculating SP from abilities');
+            return sum;
+        };
+        $scope.getUsedErf = function () {
+            var sum = 0;
+            for (skill in $scope.activeCharacter.skills) {
+                sum += $scope.getSkillUsedErf($scope.activeCharacter.skills(skill));
+            }
+            return sum;
+        };
+        $scope.getSkillUsedErf = function (skill) {
+            var sum = 0;
+            if (skill.valueErf > 0) {
+                var timesGE = skill.natural ? 1 : 0;
+                timesGE += skill.valueSp + skill.valueSpFree;
+                var fromGE = timesGE * (skill.attrPrim.value + skill.attrPrim.mod);
+                var fromTrain = skill.valueErf;
+                var total = fromGE + fromTrain;
+                if (total < 85) {
+                    sum += skill.valueErf;
+                } else if (total > 100) {
+                    if (fromGE < 85) {
+                        sum += (85 - fromGE);
+                        sum += ((100 - 85) * 2);
+                        sum += ((total - 100) * 3);
+                    } else if (fromGE > 100) {
+                        sum += ((total - fromGE) * 3);
+                    } else { //GE between..
+                        sum += ((100 - fromGE) * 2);
+                        sum += ((total - 100) * 3);
+                    }
+                } else { //between
+                    if (fromGE < 85) {
+                        sum += (85 - fromGE);
+                        sum += ((total - 85) * 2);
+                    } else {
+                        sum += ((total - fromGE) * 2);
+                    }
+                }
+            }
+            return sum;
         };
     }]);
