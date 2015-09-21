7124c33a9b6dc3fed0732ad8c9461063dc0e99cc
jens brimberg <jens.brimberg@gmail.com>
2015-09-21 19:33:56 +0200

    Added milestone overview to first page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,27 @@
  * Controller of the chargenNgApp
  */
 angular.module('chargenNgApp')
-    .controller('MainCtrl', function () {
+	.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+		var getProgress = function (milestone) {
+			if (milestone === undefined) {
+				return 0;
+			} else if (milestone.closed_issues === 0) {
+				return 0;
+			} else if (milestone.open_issues === 0) {
+				return 100;
+			} else {
+				return milestone.open_issues / (milestone.open_issues + milestone.closed_issues) * 100;
+			}
 
-    });
+		};
+		$http.get('https://api.github.com/repos/jensim/chargen-ng/milestones')
+			.then(function (res) { //SUCCESS
+				$scope.milestones = res.data;
+				var m;
+				for (m in $scope.milestones) {
+					$scope.milestones[m].progress = getProgress($scope.milestones[m]);
+				}
+			}, function (res) { //FAIL
+				$scope.milestones = [];
+			});
+    }]);
